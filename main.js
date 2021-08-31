f31f847d324eddf5667cfb44bab3a68feed4d653
banana0000 <koveszter7@gmail.com>
2021-08-31 11:47:06 +0200

    add changes

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-$(document).ready(function () {
-    $("#start1").click(function () {
-      let myCounter = setInterval(function () {
-        $("#counter").html(function (i, val) {
-          return +val + 1;
+$(document).ready(function() {
+    $("#start1").click(function() {
+        let myCounter = setInterval(function() {
+            $("#counter").html(function(i, val) {
+                return +val + 1;
+            });
+        }, 1000);
+        $("#counter").css("color", "cyan");
+        $("#stop1").click(function() {
+            clearInterval(myCounter);
         });
-      }, 1000);
-      $("#counter").css("color", "lime");
-      $("#stop1").click(function () {
-        clearInterval(myCounter);
-      });
     });
-  
-    $("#start2").click(function () {
-      let myCounter = setInterval(function () {
-        $("#counter").html(function (i, val) {
-          return +val - 1;
+
+    $("#start2").click(function() {
+        let myCounter = setInterval(function() {
+            $("#counter").html(function(i, val) {
+                return +val - 1;
+            });
+        }, 1000);
+        $("#counter").css("color", "blue");
+        $("#stop2").click(function() {
+            clearInterval(myCounter);
         });
-      }, 1000);
-      $("#counter").css("color", "green");
-      $("#stop2").click(function () {
-        clearInterval(myCounter);
-      });
     });
-  
-    $("#reset").click(function () {
-      $("#counter").html("0");
+
+    $("#reset").click(function() {
+        $("#counter").html("0");
+        $("#counter").css("color", "white");
     });
-  });
-  
+});
